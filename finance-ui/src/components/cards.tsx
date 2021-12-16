f831ad5024e393124b9eb0bd8f14283c9175211b
Deepak T <dthayyil@gmail.com>
2021-12-16 10:27:09 +0530

    Pagination for the cards

--- finance-ui/src/components/cards.tsx
+++ finance-ui/src/components/cards.tsx
@@ -1,17 +1,37 @@
-import { Grid, Paper } from '@mui/material';
+import { Grid, Pagination, Paper } from '@mui/material';
+import { useState } from 'react';
 import { history } from '../models/history'
 import { CardView } from './card';
+import usePagination from './paginations';
 
 export const Cards = ({ data }: props) => {
+    let [page, setPage] = useState<number>(1);
+    const PER_PAGE = 24;
+
+    const count = Math.ceil(data.length / PER_PAGE);
+    const _DATA = usePagination<history>(data, PER_PAGE);
+
+    const handleChange = (_e: any, p: number) => {
+        setPage(p);
+        _DATA.jump(p);
+    };
     return (
         <Grid item xs={12}> 
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                     <Grid container >
-                        {data.map(x => ( 
-                            <CardView data={x} key={x.date.valueOf()} /> 
-                        ))}
-                       </Grid>
-                </Paper> 
+            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                <Pagination count={count}
+                    size="large"
+                    page={page}
+                    variant="outlined"
+                    shape="rounded"
+                    onChange={handleChange}
+                    style={{marginBottom:10}}
+                />
+                <Grid container >
+                    {_DATA?.currentData()?.map(x => (
+                        <CardView data={x} key={x.date.valueOf()} />
+                    ))}
+                </Grid>
+            </Paper>
         </Grid>
     );
 }
